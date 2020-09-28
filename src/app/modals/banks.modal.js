abb7cd91f9cc5c477129403eabb43f41ea7d1a32
aitizaz <atz.softprgmr@gmail.com>
2020-09-28 13:06:13 +0500

    added controllers and modals of banks and users

--- src/app/modals/banks.modal.js
+++ src/app/modals/banks.modal.js
@@ -1,14 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const banksSchema = mongoose.Schema({
+  bankName: { type: String },
+  productName: { type: String },
+  sector: { type: String },
+  purpose: [{ type: String }],
 
+  markUp: {
+    type: String,
+    postFix: { type: String },
+    preFix: { type: String },
+    minMarkUp: { type: String },
+    maxMarkUp: { type: String },
+  },
+  loanAmount: {
+    minAmount: { type: Number },
+    maxAmount: { type: Number },
+  },
 
+  tenure: {
+    mintenure: { type: Number },
+    maxtenure: { type: Number },
+  },
+  collateral: [{ type: String }],
+  contactDetails: {
+    name: { type: String },
+    designation: { type: String },
+    department: { type: String },
+    address: { type: String },
+    city: { type: String },
+    phone: { type: String },
+  },
+});
 
-    
-})
-
-const Banks = mongoose.model('banks', userSchema);
+const Banks = mongoose.model("banks", banksSchema);
 
 module.exports = {
-    Banks
-}
+  Banks,
+};
